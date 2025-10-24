876c954b5604adc2b16cfc7642e27739ed89caa2
Salvatore Saieva <Sal@Saieva.com>
2025-10-24 13:15:01 -0400

    additional validations and tests for longFormatDate.

--- javascript/HelloDate.js
+++ javascript/HelloDate.js
@@ -6,9 +6,29 @@
 
 class HelloDate {
 
+   #date = undefined
+   #longFormatOptions = { weekday: 'long', month: 'long', day: '2-digit', year: 'numeric',
+                          hour: '2-digit', minute: '2-digit', second: '2-digit' };
+
+   constructor() {
+      this.#date = new Date();
+   };
+
+   get year() {
+      return this.#date.getFullYear();
+   };
+
+   get month() {
+      return this.#date.getMonth() + 1;
+   };
+
+   get day() {
+      return this.#date.getDate();
+   };
+
    get longFormatDate() {
-      return new Date();
-   }
+      return this.#date.toLocaleString('en-US', this.#longFormatOptions);
+   };
 };
 
 export default HelloDate;
--- javascript/HelloDate.test.js
+++ javascript/HelloDate.test.js
@@ -6,8 +6,26 @@ import { test } from 'node:test';
 import assert from 'node:assert';
 import HelloDate from './HelloDate.js';
 
-test('when longFormatDate is called, a string is returned', () => {
+test('when year is called, an integer is returned', () => {
    const hd = new HelloDate();
-   assert.notStrictEqual(hd.longFormatDate, null);
-   assert.notStrictEqual(hd.longFormatDate, '');
+   assert.strictEqual(Number.isInteger(hd.year), true, 'year must be an integer');
+});
+
+test('when month is called, valid value is returned', () => {
+   const hd = new HelloDate();
+   assert.strictEqual(Number.isInteger(hd.month), true, 'month must be an integer');
+   assert.ok(hd.month >= 1 && hd.month <= 12, 'month must be between 1 and 12');
+});
+
+test('when day is called, valid value is returned', () => {
+   const hd = new HelloDate();
+   assert.ok(hd.day >= 1 && hd.day <= 31, 'day must be between 1 and 31');
+});
+
+test('when longFormatDate is called, string value is validated', () => {
+   const hd = new HelloDate();
+   assert.notStrictEqual(hd.longFormatDate, null, 'longFormatDate should not be null');
+   assert.notStrictEqual(hd.longFormatDate, '', 'longFormatDate should not be the empty string');
+   assert.ok(hd.longFormatDate.includes(hd.year.toString()), 'longFormatDate should contain the year');
+   assert.ok(hd.longFormatDate.includes(hd.day.toString()), 'longFormatDate should contain the day');
 });
