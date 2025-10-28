14ad00aaf9fd2023a8279372fb3d0ef25b16153b
Salvatore Saieva <Sal@Saieva.com>
2025-10-28 15:32:15 -0400

    add logging of request.

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ import express from 'express';
 import createHomepageTemplate from './views/index.js';
 import getWhen from './views/when.js';
 
+function logRequest(method, path, ip) {
+   const datetimeFormat = { year: 'numeric', month: '2-digit', day: '2-digit',
+			    hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
+   console.log(method + ' ' + path + ' from ' + ip + ' at ' + new Date().toLocaleString('en-US', datetimeFormat) + '...');
+};
+
 const app = express();
 app.use(express.urlencoded( {extended: false} ));
 app.use(express.static('public'));
@@ -15,10 +21,12 @@ app.use(express.static('public'));
 //
 
 app.get('/', (req, res) => {
+   logRequest(req.method, req.path, req.ip);
    res.send(createHomepageTemplate());
 });
 
 app.get('/when', (req, res) => {
+   logRequest(req.method, req.path, req.ip);
    res.send(getWhen());
 });
 
